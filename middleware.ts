60b9b6e120370f4b3048eee5e4217395e8d2437c
Julian Jorgensen <jj@julianjorgensen.com>
2023-11-03 14:29:13 +0100

    add auth as middleware wrapper

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,10 @@
-export { auth as middleware } from "auth"
+import { auth } from 'auth';
+
+export default auth((req) => {
+  console.log('req.cookies', req.cookies); // returns undefined
+  return req;
+});
+
+export const config = {
+  matcher: ['/((?!api|_next|assets|monitoring|.*\\..*).*)'],
+};
